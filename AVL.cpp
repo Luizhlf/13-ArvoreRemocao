747f77ba8bcfce08248801e2110e0a6d65388366
Luizhlf <luiz301202lh@gmail.com>
2025-05-20 10:59:25 -0300

    exercicio 13

--- AVL.cpp
+++ AVL.cpp
@@ -262,6 +262,7 @@ NO* removerArvore(NO* no, int valor) {
         cout << "Elemento NAO encontrado.\n";
         return no;
     }
+
     if (valor < no->valor) {
         no->esq = removerArvore(no->esq, valor);
     }
@@ -269,21 +270,44 @@ NO* removerArvore(NO* no, int valor) {
         no->dir = removerArvore(no->dir, valor);
     }
     else {
-                
-        // Caso 1: Nó sem filhos
-        // Se o nó não possui filhos (esquerda e direita são NULL), basta removê-lo e retornar NULL para o pai.
+        // caso 1: sem filhos
+        if (no->esq == NULL && no->dir == NULL) {
+            free(no);
+            return NULL;
+        }
 
-        // Caso 2: Nó com apenas um filho
-        // Se o nó possui apenas um filho (esquerda ou direita), retorna o ponteiro para esse filho, liberando o nó atual.
+        // caso 2: um filho
+        else if (no->esq == NULL) {
+            NO* filho = no->dir;
+            free(no);
+            return filho;
+        }
+        else if (no->dir == NULL) {
+            NO* filho = no->esq;
+            free(no);
+            return filho;
+        }
 
-        // Caso 3: Nó com dois filhos
-        // Se o nó possui dois filhos, encontra o sucessor (menor valor da subárvore direita),
-        // copia o valor do sucessor para o nó atual e remove recursivamente o sucessor.
+        // caso 3: dois filhos
+        else {
+            // Procura o elemento mais à esquerda da subárvore da direita
+            NO* sucessor = no->dir;
+            while (sucessor->esq != NULL) {
+                sucessor = sucessor->esq;
+            }
+
+            // Copia o valor do sucessor para o nó atual
+            no->valor = sucessor->valor;
+
+            // Remove o sucessor
+            no->dir = removerArvore(no->dir, sucessor->valor);
+        }
     }
-    // Atualiza altura e balanceia
+
     no->altura = maior(alturaNo(no->esq), alturaNo(no->dir)) + 1;
     return balancearNo(no);
 }
+
 
 // ---------- Utilidades ----------
 int elementosArvore(NO* no) {
